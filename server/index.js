ed8dda716f3196510dc676767047e4d3a4186831
Sri <skboinapa@gmail.com>
2018-11-27 14:49:11 -0500

    index fix

--- server/index.js
+++ server/index.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
     res.send({hi:'there'});
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000 ,function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
